56f1aef0468a535586d5ac2206ca306f74914700
Paul Gomersbach <p.gomersbach@rely.nl>
2016-08-09 12:02:23 +0000

    update boot scripts

--- files/bootme.sh
+++ files/bootme.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #
-# This script installs puppet 3.x or 4.x and deploy the manifest using puppet apply -e "include demo_mco_server"
+# This script installs puppet 3.x or 4.x and deploy the manifest using puppet apply -e "include demo-mco-server"
 #
 # Usage:
-# Ubuntu / Debian: wget https://raw.githubusercontent.com/pgomersbach/demo_mco_server/master/files/bootme.sh; bash bootme.sh
+# Ubuntu / Debian: wget https://raw.githubusercontent.com/pgomersbach/demo-mco-server/master/files/bootme.sh; bash bootme.sh
 #
-# Red Hat / CentOS: curl https://raw.githubusercontent.com/pgomersbach/demo_mco_server/master/skeleton/files/bootme.sh -o bootme.sh; bash bootme.sh
+# Red Hat / CentOS: curl https://raw.githubusercontent.com/pgomersbach/demo-mco-server/master/files/bootme.sh -o bootme.sh; bash bootme.sh
 # Options: add 3 as parameter to install 4.x release
 
 # default major version, comment to install puppet 3.x
 PUPPETMAJORVERSION=4
-
+export DEBIAN_FRONTEND=noninteractive
 ### Code start ###
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root." >&2
@@ -40,33 +40,32 @@ if which apt-get > /dev/null 2>&1; then
     echo "Using yum"
 fi
 
-apt-get install git bundler zlib1g-dev -y || yum install -y git bundler zlib-devel
+apt-get install git bundler zlib1g-dev libaugeas-ruby -y -q || yum install -y git bundler zlib-devel
 
 # get or update repo
-if [ -d /root/demo_mco_server ]; then
+if [ -d /root/demo-mco-server ]; then
   echo "Update repo"
-  cd /root/demo_mco_server
+  cd /root/demo-mco-server
   git pull
 else
   echo "Cloning repo"
-  git clone https://github.com/pgomersbach/demo-mco-server.git /root/demo_mco_server
-  cd /root/demo_mco_server
+  git clone https://github.com/pgomersbach/demo-mco-server.git demo-mco-server
+  cd /root/demo-mco-server
 fi
 
-# install puppet if not installed
-if which puppet > /dev/null 2>&1; then
-   echo "Puppet is already installed."
-  else
-    bash /root/demo_mco_server/files/bootstrap.sh $PUPPETMAJOR
-fi
+# install puppet
+bash /root/demo-mco-server/files/bootstrap.sh $PUPPETMAJOR
 
 # prepare bundle
 echo "Installing gems"
-bundle install --path vendor/bundle --without development system_tests
+/opt/puppetlabs/puppet/bin/gem install puppetlabs_spec_helper --no-rdoc --no-ri -q
+
 # install dependencies from .fixtures
 echo "Preparing modules"
-bundle exec rake spec_prep
+/opt/puppetlabs/puppet/bin/rake spec_prep
+
 # copy to puppet module location
-cp -a /root/demo_mco_server/spec/fixtures/modules/* $MODULEDIR
+cp -a /root/demo-mco-server/spec/fixtures/modules/* $MODULEDIR
+
 echo "Run puppet apply"
-puppet apply -e "include demo_mco_server"
+/usr/local/bin/puppet apply -e "include demo-mco-server"
